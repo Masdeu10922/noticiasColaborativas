a57ff772d91f8b9ab82eec310289ffaff0e4aa18
Alejandro Brid <alejandrojbrid@gmail.com>
2023-09-14 12:59:12 +0200

    arreglosMail

--- src/controllers/users/editUserEmailController.js
+++ src/controllers/users/editUserEmailController.js
@@ -2,13 +2,22 @@
 const updateUserEmailModel = require('../../models/users/updateUserEmailModel');
 
 // Importamos los errores.
-const { missingFieldsError } = require('../../services/errorService');
+const {
+    missingFieldsError,
+    customError,
+} = require('../../services/errorService');
 
 const editUserEmailController = async (req, res, next) => {
     try {
         const { oldEmail, newEmail } = req.body;
         if (!oldEmail || !newEmail) {
             missingFieldsError();
+        }
+        if (oldEmail === newEmail) {
+            // Agrega una validación para verificar que el nuevo correo no sea igual al antiguo
+            customError(
+                'El nuevo correo electrónico debe ser diferente al antiguo.'
+            );
         }
 
         await updateUserEmailModel(oldEmail, newEmail, req.user.id);
